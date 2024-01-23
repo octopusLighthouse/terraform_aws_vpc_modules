a86e35cb0ff8111605d4f27026240e0b2e1d5cbd
Robertas Bauras <38780541+octopusLighthouse@users.noreply.github.com>
2024-01-23 13:08:03 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      configuration_aliases = [
+        aws.root,
+      ]
+    }
+  }
+}
+
+data "aws_availability_zones" "available" {
+    provider = aws.root
+}
+
+resource "aws_vpc" "main" {
+    provider = aws.root
+    cidr_block = var.vpc_cidr
+    tags = {
+      Name = "${var.env}-vpc-monica"
+    }
+}
+
+resource "aws_subnet" "private_subnets" {
+    provider = aws.root
+    count = length(var.private_subnet_cidrs)
+    vpc_id = aws_vpc.main.id
+    cidr_block = element(var.private_subnet_cidrs, count.index)
+    availability_zone = data.aws_availability_zones.available.names[count.index]
+    tags = {
+      Name = "${var.env}-private-subnet-monica-${count.index + 1}"
+    }
+}
